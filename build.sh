96f598f0078feafe20d7260e0e2c82bfbb1992ef
Ruofeng Lei <ruofenglei@outlook.com>
2019-09-26 23:46:47 +0800

    update: build files

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-CGO_ENABLED=0
-go build -o ./build/v2ray-exporter.linux-amd64
-GOOS=linux GOARCH=amd64 go build -o ./build/v2ray-exporter.linux-amd64
-GOOS=darwin GOARCH=amd64 go build -o ./build/v2ray-exporter.darwin-amd64
+CGO_ENABLED=0 go build -o ./build/v2ray_exporter
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./build/v2ray_exporter.linux-amd64
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./build/v2ray_exporter.darwin-amd64
